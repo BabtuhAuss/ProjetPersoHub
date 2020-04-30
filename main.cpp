4401dda1a498cc2c8bfcf9ae7672b1fb85b57667
Baptiste Aussenac <aussenacb@gmail.com>
2020-04-30 17:49:10 +0200

    debut de l'allocation de tableau

--- main.cpp
+++ main.cpp
@@ -2,17 +2,35 @@
 #include <vector>
 #include <string>
 
+#include "main.h"
+
 using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    int i = 0;
+    t_tableau tab = alloue_tableau(5);
+}
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-        i++;
-    }
-    cout << endl;
+t_tableau alloue_tableau(int n)
+{
+    t_tableau t;
+    t.nb_elem = n;
+    t.tab = (int *)malloc(sizeof(int) * n);
+    return t;
+}
+void destruct_tableau(t_tableau tab)
+{
+}
+void init_tableau(t_tableau tab)
+{
+}
+void display_tableau(t_tableau tab)
+{
+}
+t_tableau double_tableau(t_tableau tab)
+{
+    t_tableau t;
+    t.nb_elem = tab.nb_elem;
+    t.tab = (int *)malloc(sizeof(int) * tab.nb_elem);
+    return t;
 }
---
+++ main.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct
+{
+    int nb_elem; //nombre d'elements
+    int * tab; //tableau en question
+} t_tableau;
+
+
+t_tableau alloue_tableau(int n);
+void destruct_tableau(t_tableau tab);
+void init_tableau(t_tableau tab);
+void display_tableau(t_tableau tab);
+t_tableau double_tableau(t_tableau tab);
